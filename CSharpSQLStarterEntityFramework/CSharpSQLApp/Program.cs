38225536e882e037de8daa68e950bfec982177df
LeoRoma <xiajtn@gmail.com>
2020-07-20 12:58:16 +0100

    CSharpSQLStarterEntityFramework added

---
+++ CSharpSQLStarterEntityFramework/CSharpSQLApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace CSharpSQLApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new NorthwindContext())
+            {
+                Console.WriteLine(db.ContextId);
+
+                foreach(var c in db.Customers)
+                {
+                    Console.WriteLine($"Customer {c.GetFullName()} had ID {c.CustomerId}");
+                }
+            }
+
+        }
+    }
+}
